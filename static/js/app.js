32d6e95223b3e2a9ca0d3a9b712c3e56929bf80f
Rizal <ichalichali@users.noreply.github.com>
2026-05-19 09:55:08 +0700

    R10: change bell notification

--- static/js/app.js
+++ static/js/app.js
@@ -343,7 +343,10 @@ function renderShell() {
       <div class="navbar-header">
         <div class="navbar-spacer"></div>
         <button class="navbar-bell" onclick="toggleAnnouncementsDropdown()" title="Announcements">
-          🔔
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+        </svg>
           <span id="announcement-badge" class="badge" style="
             position: absolute;
             top: -8px;
